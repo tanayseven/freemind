3aa8fd0224007312f6c598ff56dc51595b1b9a2f
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-10-23 02:06:33 +0530

    Add eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,37 @@
 import eslintPluginSvelte from 'eslint-plugin-svelte';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import eslintPluginPrettierRecommended from 'eslint-config-prettier';
+import svelteEslintParser from 'svelte-eslint-parser';
+import svelteConfig from './svelte.config.js'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 export default [
-    ...eslintPluginSvelte.configs['flat/recommended'],
-    ...eslintPluginSvelte.configs['flat/prettier'],
-    {
-        rules: {
-            semi: "error",
-            "prefer-const": "error"
-        }
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  ...eslintPluginSvelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      parser: svelteEslintParser,
+      parserOptions: {
+        parser: typescriptEslintParser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.svelte'],
+        svelteConfig
+      },
     }
+  },
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: [
+      'build/',
+      'src-tauri/target/',
+      '*.config.js',
+      '.svelte-kit/'
+    ]
+  }
 ]
